2c4524ebc29f2335471b342ce5e851a6f71a3668
Ahmad Amine Zrouli <99292653+zrouliaa@users.noreply.github.com>
2023-03-09 20:37:21 +0100

    Update register.php

--- backend/register.php
+++ backend/register.php
@@ -16,10 +16,10 @@
       //Server settings
       $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
       $mail->isSMTP();                                            //Send using SMTP
-      $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+      $mail->Host       = 'smtp.gmail.com';                       //Set the SMTP server to send through
       $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-      $mail->Username   = 'zrouli.aa@gmail.com';                     //SMTP username
-      $mail->Password   = 'aeumblkhgueegvct';                               //SMTP password
+      $mail->Username   = 'your_email';                           //SMTP username
+      $mail->Password   = 'your_email_password';                  //SMTP password
       $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
       $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
   
